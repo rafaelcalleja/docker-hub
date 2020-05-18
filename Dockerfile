9626930cd35a4af2fd7c9252c81cbee0645bc140
Rafael Calleja <rafaelcalleja@gmail.com>
2020-05-18 09:12:49 +0200

    add docker in qemu

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,21 @@
 FROM ubuntu
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && \
+    apt-get install -y qemu \
+    cloud-image-utils && \
+    rm -rf /var/lib/apt/lists/* && \
+    apt-get clean
+
+WORKDIR /root
+COPY ubuntu-18.04-server-cloudimg-amd64.img /root
+
+COPY start-qemu.sh /usr/local/bin/
+COPY cloud-init-user-data.yaml /root/
+
+CMD ["/usr/local/bin/start-qemu.sh"]
+
+#docker run --privileged --cap-add=ALL --device /dev/kvm --mount type=tmpfs,destination=/var/tmp -p 5555:5555 -p 2378:2378 -v /dev/shm:/dev/shm -it --rm -d rafa/qemu
+
+#docker run --device /dev/kvm --mount type=tmpfs,destination=/var/tmp -p 5555:5555 -v /dev/shm:/dev/shm -d -it --rm rafa/qemu
---
+++ update-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -a
+LIBGUESTFS_BACKEND=
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --root-password password:ubuntu
+
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --install apt-transport-https,ca-certificates,curl,gnupg-agent,software-properties-common
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -"
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command 'apt-get update -y'
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command 'apt-get install -y docker-ce docker-ce-cli containerd.io'
+
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command 'systemctl enable docker'
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command 'mkdir /etc/docker'
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --copy-in daemon.json:/etc/docker
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command "mkdir -p /etc/systemd/system/docker.service.d"
+sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --run-command "echo '[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd' > /etc/systemd/system/docker.service.d/simple_dockerd.conf"
+
